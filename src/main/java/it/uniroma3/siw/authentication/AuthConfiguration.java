bb4c0a804aa7862866dd7b1d1f397cd9a43e75eb
hermann0001 <her.tamilia@stud.uniroma3.it>
2023-06-19 15:55:47 +0200

    Implemented css and js, with bootstrap libraries.

--- src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -50,14 +50,14 @@ import javax.sql.DataSource;
         httpSecurity
                 .csrf().and().cors().disable()
                 .authorizeHttpRequests()
-//                .requestMatchers("/**").permitAll()
-                // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
-                .requestMatchers(HttpMethod.GET,"/","/index","/register","/css/**", "/images/**", "favicon.ico", "/movie/**", "/artist/**").permitAll()
+                //.requestMatchers("/**").permitAll()
+                // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css, ai js, a bootstrap e alle immagini
+                .requestMatchers(HttpMethod.GET,"/","/index","/register","/css/**", "/images/**", "favicon.ico", "/movie/**", "/movies/**", "/artist/**", "/js/**", "/error", "/vendor/**").permitAll()
         		// chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register 
                 .requestMatchers(HttpMethod.POST,"/register", "/login").permitAll()
                 .requestMatchers(HttpMethod.GET,"/admin/**").hasAnyAuthority(ADMIN_ROLE)
                 .requestMatchers(HttpMethod.POST,"/admin/**").hasAnyAuthority(ADMIN_ROLE)
-        		// tutti gli utenti autenticati possono accere alle pagine rimanenti 
+        		// tutti gli utenti autenticati possono accedere alle pagine rimanenti
                 .anyRequest().authenticated()
                 // LOGIN: qui definiamo il login
                 .and().formLogin()
